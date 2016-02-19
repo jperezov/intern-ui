ea88efffb969fa64e3795adf423d3b538fe0a25e
Jonathan Perez <perezj@fiu.edu>
2016-02-19 14:01:51 -0500

    Fixed installation script.

--- installation.js
+++ installation.js
@@ -1,2 +1,5 @@
 // Kinda pointless, but if it makes installation easier...
-require('child_process').exec("bower install && grunt");
+require('child_process').exec("cd node_modules/intern-ui && bower install && grunt", function(error, stdout) {
+    error && console.log(error);
+    stdout && console.log(stdout);
+});
